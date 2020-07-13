17e2135705cad486ba3987d387510bf4ffdc980b
sudo-julia <jlearning@tuta.io>
2020-07-13 02:26:45 -0700

    fixing get-installed and relocating the 'u' date check

--- get-installed.sh
+++ get-installed.sh
@@ -7,6 +7,6 @@ mkdir "$DIR"
 pacman -Q > "$DIR"/all
 cut -d' ' -f1 "$DIR"/all > "$DIR"/no_version
 pacman -Qm | cut -d' ' -f1 > "$DIR"/aur
-diff -y "$DIR"/no_version "$DIR"/aur | awk '{print $2}' | sed -r '/^\s*$/d' > "$DIR"/no_aur
+diff -y "$DIR"/no_version "$DIR"/aur | awk '{print $2}' | sed '/</d' > "$DIR"/no_aur
 printf "Installed programs backed up to %s\n" "$DIR"
 
