2bd523ed4ed67bf4242f1020f2dfd8ff58e02d9c
sudo-julia <jlearning@tuta.io>
2020-08-09 16:47:28 -0700

    mkbak script now provides usage

--- mkbak.sh
+++ mkbak.sh
@@ -4,17 +4,28 @@
 #TODO split this var into lines and find way to avoid var alltogether
 mkrt="^/bin|^/boot|^/dev|^/etc|^/home$|^/mnt|^/opt|^/proc|^/run|^/root|^/srv|^/sys|tmp|^/usr|^/var"
 
-if [ "$#" -eq 0 ]; then
-	readlink -e "$HOME"/{,.}*/{,*}/{,*}/{,*} \
-	| grep -Evi "chromium|^/lost\+found|$mkrt" \
-	| fzf -m --height=50% --border --prompt='backup: ' --marker='*' \
-	| xargs -P 4 -I % sh -c "cp -a -- % %.bak && echo 'created %.bak'"
-fi
+main () {
+	if [ "$#" -eq 0 ]; then
+		readlink -e "$HOME"/{,.}*/{,*}/{,*}/{,*} \
+		| grep -Evi "chromium|^/lost\+found|$mkrt" \
+		| fzf -m --height=50% --border --prompt='backup: ' --marker='*' \
+		| xargs -P 4 -I % sh -c "cp -a -- % %.bak && echo 'created %.bak'"
+	fi
 
-while (( $# )); do
-	cp -a -- "$1" "${1}.bak"
-	echo "created ${1}.bak"
-	shift
-done
+	while (( $# )); do
+		cp -a -- "$1" "${1}.bak"
+		echo "created ${1}.bak"
+		shift
+	done
+}
 
-(exit 0)
+case $1 in
+	-h|--help)
+		printf "Usage: mkbak.sh [FILE]\n"
+		printf "If no arguments are given, mkbak will search for files to backup in \$HOME.\n"
+		printf "If arguments are given, mkbak will make copie(s) of any file(s) given with the extension '.bak'.\n"
+		;;
+	*)
+		main "$@" && (exit 0)
+		;;
+esac
