dd79bedd7d01b518807c4bcd5a0975736766347e
sudo-julia <jlearning@tuta.io>
2020-10-03 17:46:34 -0700

    git support added to getInstalled for better hist

--- getInstalled.bash
+++ getInstalled.bash
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 # send lists of installed programs to dir specified by date
 
+user=jam
+BASEDIR=/home/"${user}"/documents/misc/installed
 DATE=$( date "+%y%m%d" )
-DIR=/home/jam/documents/misc/Installed/${DATE}
+DIR=/home/"${user}"/documents/misc/installed/"${DATE}"
 
+# TODO make something to increment this instead of a one time fix
 if [[ -d "${DIR}" ]]; then
-	DIR=/home/jam/documents/misc/Installed/"${DATE}".1
+	DIR=/home/"${user}"/documents/misc/installed/"${DATE}".1
+fi
+
+if [[ ! -d "${BASEDIR}"/.git ]]; then
+	( cd "${BASEDIR}" && git init )
 fi
 
 mkdir "${DIR}"
@@ -13,4 +20,5 @@ pacman -Q > "${DIR}"/all
 cut -d' ' -f1 "${DIR}"/all > "${DIR}"/no_version
 pacman -Qm | cut -d' ' -f1 > "${DIR}"/aur
 diff -y "${DIR}"/no_version "${DIR}"/aur | awk '{print $2}' | sed '/</d' > "${DIR}"/no_aur
+( cd "${BASEDIR}" && git add . && git commit -m "${user} $( date '+%c' )")
 printf -- '%s %s\n' "Installed programs backed up to" "${DIR}"
