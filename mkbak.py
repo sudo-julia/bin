5f5485ad6c1a3aea4946ebd5efb07a47be9fee3f
sudo-julia <jlearning@tuta.io>
2020-12-11 12:22:32 -0800

    height option added, type annotations improved

--- mkbak.py
+++ mkbak.py
@@ -3,11 +3,11 @@ import argparse
 import os
 import shutil
 import stat
-from typing import Generator
+from typing import Generator, Optional, Tuple
 from iterfzf import iterfzf
 
 
-__version__ = "v0.3.4"
+__version__ = "v0.4.0"
 
 
 def copy_all(file: str, location: str):
@@ -23,7 +23,9 @@ def copy_all(file: str, location: str):
     os.chown(location, st[stat.ST_UID], st[stat.ST_GID])
 
 
-def iterate_files(path: str, filetype: str, hidden=False) -> Generator[str, None, None]:
+def iterate_files(
+    path: str, filetype: Optional[str], hidden=False
+) -> Generator[str, None, None]:
     """iterate through files as DirEntries to feed to fzf wrapper"""
     with os.scandir(path) as it:
         if filetype:
@@ -48,14 +50,27 @@ def iterate_files(path: str, filetype: str, hidden=False) -> Generator[str, None
 
 
 def main():
-    filetype, exact, hidden, ignore, path, preview, recurse, verbose = parse_args()
+    (
+        exact,
+        filetype,
+        height,
+        hidden,
+        ignore,
+        path,
+        preview,
+        recurse,
+        verbose,
+    ) = parse_args()
 
+    # TODO is there a way to store options in a tuple and unload them into
+    #      both functions?
     if not recurse:
         files = iterfzf(
             iterable=(iterate_files(path, filetype, hidden)),
             case_sensitive=ignore,
             exact=exact,
             encoding="utf-8",
+            height=height,
             preview=preview,
             multi=True,
         )
@@ -65,6 +80,7 @@ def main():
             case_sensitive=ignore,
             exact=exact,
             encoding="utf-8",
+            height=height,
             preview=preview,
             multi=True,
         )
@@ -81,7 +97,9 @@ def main():
     exit(0)
 
 
-def parse_args():
+def parse_args() -> Tuple[
+    bool, Optional[str], str, bool, bool, str, Optional[str], bool, bool
+]:
     """parse arguments fed to script and set options"""
     # TODO make extension copying recursive
     # TODO arg addition to recursive that allows for depth to recurse
@@ -92,23 +110,38 @@ def parse_args():
     main_args.add_argument(
         "-a", "--all", help="show hidden and 'dot' files", action="store_true"
     )
+    # TODO exact broke at filetype in iterate
     matching_group.add_argument(
         "-e", "--exact", help="exact matching", action="store_true"
     )
     matching_group.add_argument(
         "-f",
         "--filetype",
+        default=None,
         help="only find files of a provided extension. recursion not supported",
         type=str,
     )
+    main_args.add_argument(
+        "--height",
+        default=100,
+        help="""display fzf window with the given height. takes an
+                           int between 0-100""",
+        type=int,
+    )
     matching_group.add_argument(
-        "-i", "--ignore_case", help="ignore case distinction", action="store_true"
+        "-i",
+        "--ignore_case",
+        help="ignore case distinction",
+        action="store_true",
     )
     main_args.add_argument(
         "-p", "--path", help="directory to run in (default './')", default=".", type=str
     )
     main_args.add_argument(
-        "--preview", help="starts external process with current line as arg", type=str
+        "--preview",
+        default=None,
+        help="starts external process with current line as arg",
+        type=str,
     )
     main_args.add_argument(
         "-r", "--recursive", help="recurse through current dir", action="store_true"
@@ -119,40 +152,26 @@ def parse_args():
     parser.add_argument("--version", help="print version number", action="store_true")
 
     args = parser.parse_args()
+
+    exact: bool = args.exact
+    filetype: Optional[str] = args.filetype
+    height: str
+    hidden: bool = args.all
+    ignore: bool = args.ignore_case
+    path: str = args.path
+    preview: Optional[str] = args.preview
+    recurse: bool = args.recursive
+    verbose: bool = args.verbose
+
     if args.version:
         print(f"mkbak.py {__version__}")
         exit(0)
-    if args.all:
-        hidden = True
+    if args.height in range(0, 101):
+        height = str(args.height) + "%"
     else:
-        hidden = None
-    if args.exact:
-        exact = True
-    else:
-        exact = False
-    if args.filetype:
-        filetype = str(args.filetype)
-    else:
-        filetype = None
-    if args.ignore_case:
-        ignore = False
-    else:
-        ignore = None
-    path = args.path
-    if args.preview:
-        preview = args.preview
-    else:
-        preview = None
-    if args.recursive:
-        recurse = True
-    else:
-        recurse = False
-    if args.verbose:
-        verbose = True
-    else:
-        verbose = False
+        height = "100%"
 
-    return filetype, exact, hidden, ignore, path, preview, recurse, verbose
+    return exact, filetype, height, hidden, ignore, path, preview, recurse, verbose
 
 
 def recursive(path: str, hidden=None) -> Generator[str, None, None]:
