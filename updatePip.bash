69fe0b9b16b3459a8bbe660e02904d7ff3500cba
sudo-julia <jlearning@tuta.io>
2020-12-21 22:54:28 -0800

    updatePip can now tell python version

--- updatePip.bash
+++ updatePip.bash
@@ -2,18 +2,23 @@
 # script to update pip, meant for integration in `u`
 # DO NOT RUN AS SUPERUSER
 
+pythonVer=$( env pip --version | grep -o '\(python [0-3]\.[0-9]\)' )
+
 if [[ "$( id -u )" == 0 ]]; then
 	printf -- '%s\n%s\n' "Error! Do not run as root." "Cancelling..."
 	exit 1
 fi
 
-if [ ! -d ~/.local/lib/python3.8/installed ]; then
-	mkdir ~/.local/lib/python3.8/installed
+if [ ! -d ~/.local/lib/"${pythonVer}"/installed ]; then
+	mkdir ~/.local/lib/"${pythonVer}"/installed
 fi
 
-# shellcheck disable=SC2046
-pip list -o | grep -v '\^-e' | tee ~/.local/lib/python3.8/installed/"$( date +\"%y%m%d\" )" \
-| grep -Ev 'sdist|--|Latest\s' | cut -d' ' -f1 | xargs -r -n1 pip install --user -U
-
-printf -- '\n%s\n' "Pip packages upgraded."
+printf -- '%s\n' "Upgrading old pip packages..."
+pip list -o \
+	| grep -v '\^-e' \
+	| tee ~/.local/lib/"${pythonVer}"/installed/"$( date +\"%Y%m%d\" )" \
+	| grep -Ev 'sdist|--|Latest\s' \
+	| cut -d' ' -f1 \
+	| xargs -r -n1 pip install --user -U
+printf -- '%s\n' "Pip packages upgraded."
 exit 0
