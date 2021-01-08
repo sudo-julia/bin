6bbb15100757f99cd9f294274d36f780acad4886
sudo-julia <jlearning@tuta.io>
2021-01-07 22:15:07 -0800

    styling overhaul

--- cleanSymlinks.sh
+++ cleanSymlinks.sh
@@ -2,18 +2,21 @@
 # list and offer to remove all broken symlinks recursing from your current dir
 # TODO checks to exclude useful broken symlinks
 
-removeSymlinks () {
+remove_symlinks() {
+	# remove all broken symlinks
 	find . -xtype l -exec rm -v {} \;
 }
 
-logSymlinks () {
-	output=$( mktemp --suffix '.txt' )
-	find . -xtype l ! -exec test -e {} \; -print > "${output}" && \
-	printf -- '%s\n' "Symlinks output to ${output} for review."
+log_symlinks() {
+	# log broken symlinks to a tmpfile
+	output="$( mktemp --suffix '.txt' )"
+	find . -xtype l ! -exec test -e {} \; -print > "${output}" \
+	&& printf -- '%s\n' "Symlinks output to ${output} for review."
 	printf -- '%s\n' "Exiting program without removing any symlinks."
 	exit 1
 }
 
+# find broken symlinks (won't work on OSX)
 find . -xtype l ! -exec test -e {} \; -print
 printf -- '%s\n' "Remove broken symlinks? [Y/n] "; read -r remove
 case "$remove" in
@@ -21,12 +24,14 @@ case "$remove" in
 		printf -- '%s\n' "Are you sure? [Y/n] "; read -r confirm
 		case "$confirm" in
 			y|Y)
-				removeSymlinks  ;;
+				remove_symlinks
+				;;
 			*)
-				logSymlinks
-		esac  ;;
+				log_symlinks
+		esac
+		;;
 	*)
-		logSymlinks
+		log_symlinks
 esac
 
 exit 0
