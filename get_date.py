34557f32300f1aebb0096012bfee16d77cc541fe
sudo-julia <jlearning@tuta.io>
2021-03-03 14:00:31 -0800

    get_date added for date-checking in updater

---
+++ get_date.py
@@ -0,0 +1,17 @@
+"""convert next date for 'u'"""
+#!/usr/bin/env python3
+import datetime
+from sys import argv
+
+
+def get_new_date(date: str):
+    """get the date 7 days from the date given
+    date must be formatted as 'YYYY-MM-DD'
+    """
+    last_day = datetime.date.fromisoformat(date)
+    next_day = last_day + datetime.timedelta(weeks=1)
+    print(next_day)
+
+
+if __name__ == "__main__":
+    get_new_date(argv[1])
